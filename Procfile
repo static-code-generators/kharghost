d23d8d33711eea9f2936de7001b0c66a5d23ac83
Jai Luthra <me@jailuthra.in>
2015-10-26 15:11:18 +0530

    Heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn kharghost.wsgi --log-file -
--- kharghost/wsgi.py
+++ kharghost/wsgi.py
@@ -13,4 +13,8 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "kharghost.settings")
 
-application = get_wsgi_application()
+# application = get_wsgi_application()
+# from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
+
+application = Cling(get_wsgi_application())
