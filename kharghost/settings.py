02ed67f74d160226279878cd1cde6ee9215732ae
Jai Luthra <me@jailuthra.in>
2015-10-26 19:17:38 +0530

    settings: Added support for local_settings

--- kharghost/settings.py
+++ kharghost/settings.py
@@ -113,11 +113,15 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 ALLOWED_HOSTS = ['*']
 
 # Static asset configuration
-import os
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'blog/static'),
 )
+
+try:
+    from local_settings import *
+except ImportError:
+    print("No local settings found")
+    pass
