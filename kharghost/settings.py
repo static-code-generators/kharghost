31a133653b922c8ab985bb5c1e2dcf7a3710049a
Jai Luthra <me@jailuthra.in>
2015-10-26 23:08:34 +0530

    DEBUG off

--- kharghost/settings.py
+++ kharghost/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '!58=@74_!#^zd9o9)ge8cy=16+r=p4=7euht%3(j9-o397!e!b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
