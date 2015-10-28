a5119fdd587c22c52c4f80b8d8037dc5e0ea8cf9
Jai Luthra <me@jailuthra.in>
2015-10-28 16:19:52 +0530

    Edited local_settings file

--- local_settings_sample.py
+++ local_settings_sample.py
@@ -1,10 +1,9 @@
 import os
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DEBUG = True
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
-# Copy this file to local_settings.py on your local machine to run using runserver command
